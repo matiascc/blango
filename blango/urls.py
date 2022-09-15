d2471e10129dba0a73dd1748daf4e1f63d01abdc
Matías Caporale <matiascaporale9@gmail.com>
2022-09-15 00:23:19 +0000

    Finish django registration

--- blango/urls.py
+++ blango/urls.py
@@ -19,6 +19,9 @@ import blog.views
 import debug_toolbar
 from django.conf import settings
 from django.urls import path, include
+import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
 
 
 urlpatterns = [
@@ -31,4 +34,13 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
+        path("accounts/", include("django.contrib.auth.urls")),
+        path("accounts/profile/", blango_auth.views.profile, name="profile"),
+        path(
+            "accounts/register/",
+            RegistrationView.as_view(form_class=BlangoRegistrationForm),
+            name="django_registration_register",
+        ),
+        path("accounts/", include("django_registration.backends.activation.urls")),
+
     ]
